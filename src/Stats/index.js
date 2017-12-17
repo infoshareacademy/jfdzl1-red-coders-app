5b28273ebe40bd401c0e0664f5832bc7a2ac5620
Jakub Fedyk <jakubfedyk@gmail.com>
2017-12-17 13:57:03 +0100

    creating separate js for charts

--- src/Stats/index.js
+++ src/Stats/index.js
@@ -1,27 +1,15 @@
 import React, {Component} from 'react';
-import {LineChart, Line} from 'recharts';
+import {ResponsiveContainer, PieChart, Pie} from 'recharts';
 import {database} from 'firebase';
 
-const data = [
-    {name: 'Day 1', uv: 11},
-    {name: 'Day 2', uv: 22},
-    {name: 'Day 3', uv: 33},
-    {name: 'Day 4', uv: 44},
-    {name: 'Day 5', uv: 55},
-    {name: 'Day 6', uv: 66},
-    {name: 'Day 7', uv: 77},
-];
-const ChartStyle= {
-    backgroundColor: 'yellow',
-};
 class Stats extends Component {
 
     render() {
         return (
             <div>
-                <LineChart style={ChartStyle} width={400} height={400}  data={data}>
-                    <Line type="monotone" dataKey="uv" stroke="#8884d8"/>
-                </LineChart>
+                <ResponsiveContainer width={700} height="80%">
+                <PieChart/>
+                </ResponsiveContainer>
             </div>
         )
     }
