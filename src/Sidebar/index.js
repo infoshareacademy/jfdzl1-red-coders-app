2a0743145c2d5688b1a4a79271d348363771d22f
Maciej Ziemba <maciek.ziemba@gmail.com>
2017-12-17 14:01:35 +0100

    Textfield added...

--- src/Sidebar/index.js
+++ src/Sidebar/index.js
@@ -1,14 +1,37 @@
 import React, {Component} from 'react';
 import Grid from 'material-ui/Grid';
+import TextField from 'material-ui/TextField';
+import Divider from 'material-ui/Divider';
 
 class Sidebar extends Component {
-  render() {
-    return (
-      <Grid item xs={12}>
-        Test sidebar
-      </Grid>
-    );
-  }
+
+    state = {
+        name: ''
+    }
+
+    handleChange = name => event => {
+        this.setState({
+            [name]: event.target.value,
+        });
+    }
+
+
+    render() {
+        return (
+            <Grid item xs={12}>
+              Test sidebar
+              <Divider />
+              Write the name a new attraction...
+              <TextField
+                  id="name"
+                  label="Name"
+                  value={this.state.name}
+                  onChange={this.handleChange('name')}
+                  margin="normal"
+              />
+            </Grid>
+        );
+    }
 }
 
 export default Sidebar;
