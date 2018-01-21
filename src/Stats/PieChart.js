f0d9eafa33d8dc1c4c721b315f7da6fbcb9dc671
Jakub Fedyk <jakubfedyk@gmail.com>
2018-01-21 13:48:01 +0100

    placement of charts fix

--- src/Stats/PieChart.js
+++ src/Stats/PieChart.js
@@ -8,13 +8,7 @@ const data02 = [{name: 'A1', value: 100},
     {name: 'A2', value: 300},
     {name: 'B1', value: 100},
     {name: 'B2', value: 80},
-    {name: 'B3', value: 40},
-    {name: 'B4', value: 30},
-    {name: 'B5', value: 50},
-    {name: 'C1', value: 100},
-    {name: 'C2', value: 200},
-    {name: 'D1', value: 150},
-    {name: 'D2', value: 50}]
+    {name: 'B3', value: 40}]
 
 
 class Chart extends Component {
